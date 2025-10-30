994988b2f9e2c183345ddc13175e86a9c173d7a8
Yaroslav Sledevskiy <yaroslavsledevskiy@MacBook-Air-Yaroslav.local>
2025-10-30 09:29:53 +0300

    feat: минимальный парсер

--- src/tg_client.py
+++
@@ -1,5 +0,0 @@
-from telethon import TelegramClient
-from .config import API_ID, API_HASH, SESSION_NAME
-
-def get_client() -> TelegramClient:
-    return TelegramClient(SESSION_NAME, API_ID, API_HASH)
